e25a092270c2327b71e0da766e8d05199fc84f04
lsieck519 <99048123+lsieck519@users.noreply.github.com>
2023-03-29 13:00:32 -0500

    more setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongodb = require("mongodb").MongoClient;
+const data = require("./models/data");
+
+const app = express();
+const port = 3001;
+
+const connectionStringURI = `mongodb://127.0.0.1:27017/socialDB`;
+
+let db;
+
+
+app.use(express.json());
+
+
+app.get("/users", (req, res) => {
+})
