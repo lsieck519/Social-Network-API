77b8fc073ac14e238ea09a443581075d08bb7d77
lsieck519 <99048123+lsieck519@users.noreply.github.com>
2023-03-31 09:52:52 -0500

    folder setup

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require("express");
-const mongodb = require("mongodb").MongoClient;
-const data = require("./models/data");
+const db = require("./config/connection");
+const routes = require("./routes");
 
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3001;
 
-const connectionStringURI = `mongodb://127.0.0.1:27017/socialDB`;
-
-let db;
-
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes)
 
-
-app.get("/users", (req, res) => {
-})
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
